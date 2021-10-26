ebb45656975352b57076aaf237431fccd1fa2dcb
Basant Besra <besrabasant@gmail.com>
2021-10-26 16:28:53 +0530

    DISable cache till feature to invalidate cache is added.

--- src/InfraStructure/Theme.php
+++ src/InfraStructure/Theme.php
@@ -4,25 +4,28 @@ namespace Workhorse\InfraStructure;
 
 use Illuminate\Cache\Repository;
 
+// TODO: DISable cache till feature to invalidate cache is added.
 class Theme {
 	public static function cache( string $key, \Closure $dataCallback, $ttl = null ) {
 		$key = function_exists( 'pll__' ) ? $key . "_" . get_locale() : $key;
 
-		if ( WP_ENV !== 'production' ) {
-			return $dataCallback();
-		}
+		return $dataCallback();
 
-		/** @var Repository $cache */
-		$cache = \Roots\app( 'cache' );
-
-		if ( ! $cache->has( $key ) ) {
-
-			$data = $dataCallback();
-
-
-			$cache->put( $key, $data, $ttl );
-		}
-
-		return $cache->get( $key );
+//		if ( WP_ENV !== 'production' ) {
+//			return $dataCallback();
+//		}
+//
+//		/** @var Repository $cache */
+//		$cache = \Roots\app( 'cache' );
+//
+//		if ( ! $cache->has( $key ) ) {
+//
+//			$data = $dataCallback();
+//
+//
+//			$cache->put( $key, $data, $ttl );
+//		}
+//
+//		return $cache->get( $key );
 	}
 }
