65aa34d5c784736b8a407cb9b0c5d4dd0a67edfe
Basant Besra <besrabasant@gmail.com>
2021-10-26 12:57:15 +0530

    Add support for translated cache content

--- src/InfraStructure/Theme.php
+++ src/InfraStructure/Theme.php
@@ -6,6 +6,8 @@ use Illuminate\Cache\Repository;
 
 class Theme {
 	public static function cache( string $key, \Closure $dataCallback, $ttl = null ) {
+		$key = function_exists( 'pll__' ) ? $key . "_" . get_locale() : $key;
+
 		if ( WP_ENV !== 'production' ) {
 			return $dataCallback();
 		}
